19301e4a3c24c559a0560f69bf5ac8545b9f1ca9
ubyhzargam <ubyhzargam@gmail.com>
2023-07-19 00:20:08 +0530

    Update main.cpp

--- reversing_a_linked_list/main.cpp
+++ reversing_a_linked_list/main.cpp
@@ -6,9 +6,68 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first=NULL,*last=NULL;
+void create(int x)
+{
+    struct Node *t=(struct Node*)malloc(sizeof(struct Node));
+    t->data=x;
+    t->next=NULL;
+    if(first==NULL)
+    {
+        first=t;
+        last=t;
+    }
+    else
+    {
+        last->next=t;
+        last=t;
+    }
+}
+void display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+    cout<<endl;
+}
+void reverse(struct Node *p)
+{
+    struct Node *p1;
+    struct Node *q;
+    q=p->next;
+    p->next=NULL;
+    while(q->next!=NULL)
+    {
+        p1=q->next;
+        q->next=p;
+        p=q;
+        q=p1;
+    }
+    q->next=p;
+    first=q;
+}
+int main()
+{
+    int n,x;
+    cout<<"Enter the number of elements in the linked list : "<<endl;
+    cin>>n;
+    cout<<"Enter the elemens of the linked list : "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        create(x);
+    }
+    cout<<"The original linked list is given by : "<<endl;
+    display(first);
+    cout<<"The reversed linked list is given by : "<<endl;
+    reverse(first);
+    display(first);
     return 0;
 }
